415a473da38982c77df0135874ca9ae8c989480f
ridwan-dana <fansurim@DNID2854L01.local>
2025-01-17 21:42:56 +0700

    add signout

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,6 +5,8 @@ import { UpdateUserDto } from "./dtos/update-user.dto";
 import { UserDto } from "./dtos/user.dto";
 import { Serialize } from "../interceptors/serialize.interceptor";
 import { AuthService } from "./auth.service";
+import { User } from "./user.entity";
+import { CurrentUser } from "./decorators/current-user-decorator";
 
 
 @Controller('auth')
@@ -26,6 +28,16 @@ export class UsersController {
   async signin(@Body() body: CreateUserDto, @Session() session: any) {
     const user = await this.authService.signin(body.email, body.password);
     session.userId = user.id;
+    return user;
+  }
+
+  @Post('/signout')
+  signOut(@Session() session: any) {
+    session.userId = null
+  }
+
+  @Get('/whoami')
+  whoAmI(@CurrentUser() user: string) {
     return user;
   }
 
@@ -55,3 +67,4 @@ export class UsersController {
 
 
 }
+
